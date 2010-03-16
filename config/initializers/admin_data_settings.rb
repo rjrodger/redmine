9b851ce3cf2dd5016b689915969962321418df49
Richard Rodger <richard@ricebridge.com>
2010-03-16 14:45:59 +0000

    removed file

--- config/initializers/admin_data_settings.rb
+++
@@ -1,5 +0,0 @@
-AdminDataConfig.set = {
-  :is_allowed_to_view => lambda {|controller| controller.send('admin_logged_in?') },
-  :is_allowed_to_update => lambda {|controller| controller.send('admin_logged_in?') },
-  :is_allowed_to_view_feed => lambda {|controller| controller.send('admin_logged_in?') }
-}
